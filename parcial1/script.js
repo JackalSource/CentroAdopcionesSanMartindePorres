9c1a65307a066703029d17ed03796d2d0886a332
JackalSource <xarkadius14@gmail.com>
2026-08-03 20:12:21 -0400

    Boton funcional

---
+++ parcial1/script.js
@@ -0,0 +1,9 @@
+function mostrarMensaje(){
+    const mensaje = document.querySelector("#mensaje");
+
+    mensaje.textContent = "Pedido recibido - te ateinde Mauricio Aparicio";
+    mensaje.classList.remove("oculto");
+}
+
+const boton = document.querySelector("#btn-confirmar");
+boton.addEventListener("click", mostrarMensaje);
