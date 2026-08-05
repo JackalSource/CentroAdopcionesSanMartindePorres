ec894e70460d989d446c2deaa69a3272153d9b15
JackalSource <xarkadius14@gmail.com>
2026-08-04 21:19:39 -0400

    Validaciones

--- script.js
+++ script.js
@@ -13,5 +13,56 @@ function alternarModo(){
         botonModo.textContent = "🌞 Modo día"
     }
 }
-
 botonModo.addEventListener("click", alternarModo);
+
+
+const formularioAdopcion = document.querySelector("#form-adopcion");
+const avisoPedido = document.querySelector("#error-pedido");
+
+function revisarPedido(event) {
+    event.preventDefault();
+
+    const nombre = document.querySelector("#nombre").value;
+    const correo = document.querySelector("#correo").value;
+    const telefono = document.querySelector("#telefono").value;
+    const mascota = document.querySelector("#mascota").value;
+    const motivo = document.querySelector("#motivo").value;
+
+    if (nombre == "") {
+        avisoPedido.textContent = "Falta tu nombre.";
+        avisoPedido.classList.add("error");
+        avisoPedido.classList.remove("exito");
+    }
+
+    else if (correo.includes("@") == false) {
+        avisoPedido.textContent = "El correo no parece correcto. Le falta el @.";
+        avisoPedido.classList.add("error");
+        avisoPedido.classList.remove("exito");
+    }
+
+    else if (telefono == "") {
+        avisoPedido.textContent = "Falta tu teléfono.";
+        avisoPedido.classList.add("error");
+        avisoPedido.classList.remove("exito");
+    }
+
+    else if (mascota == "Seleccione una opción") {
+        avisoPedido.textContent = "Seleccione una mascota.";
+        avisoPedido.classList.add("error");
+        avisoPedido.classList.remove("exito");
+    }
+
+    else if (motivo == "") {
+        avisoPedido.textContent = "Escriba el motivo de la adopción.";
+        avisoPedido.classList.add("error");
+        avisoPedido.classList.remove("exito");
+    }
+
+    else {
+        avisoPedido.textContent = "Solicitud enviada correctamente. Nos pondremos en contacto contigo.";
+        avisoPedido.classList.add("exito");
+        avisoPedido.classList.remove("error");
+    }
+}
+
+formularioAdopcion.addEventListener("submit", revisarPedido);
