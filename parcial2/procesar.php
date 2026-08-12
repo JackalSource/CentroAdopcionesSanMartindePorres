e465a01fddc3d4c3c2641f5beb1dc677ce1b481e
JackalSource <xarkadius14@gmail.com>
2026-08-11 20:04:06 -0400

    Script y procesar funcionales

---
+++ parcial2/procesar.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "<h1>Su pedido fue recibido en nuestra heladeria Doña Nieve</h1>";
+
+echo "<p>Nombre: " . $_POST["nombre"] . "</p>";
+echo "<p>Correo: " . $_POST["correo"] . "</p>";
+echo "<p>Sabores: " . $_POST["sabores"] . "</p>";
+
+$productos = [
+    "Como simple - Bs 8",
+    "Copa doble - Bs 15",
+    "Litro para llevar - Bs 35"
+];
+
+echo "<h2>Carta de la heladeria</h2>";
+
+echo "<ul>";
+
+foreach ($productos as $producto) {
+    echo "<li>" . $producto . "</li>";
+}
+
+echo "</ul>";
+
+echo "<p>Te aitende Mauricio Gustavo Aparicio Muñoz</p>";
+
+?>
---
+++ parcial2/script.js
@@ -0,0 +1,34 @@
+const formularioHelados = document.querySelector("#form-helados");
+const avisoHelados = document.querySelector("#aviso-helados");
+
+function revisarPedido(event) {
+    const nombre = document.querySelector("#nombre").value;
+    const correo = document.querySelector("#correo").value;
+    const sabores = document.querySelector("#sabores").value;
+
+    if (nombre == "" || correo == "") {
+
+        event.preventDefault();
+
+        avisoHelados.textContent = "Falta tu nombre otu correo, debes llenar los campos para realiza pedido";
+        avisoHelados.classList.add("error");
+        avisoHelados.classList.remove("exito");
+
+    } else if (correo.includes("@") == false) {
+
+        event.preventDefault();
+
+        avisoHelados.textContent = "El correo es incorrecto";
+        avisoHelados.classList.add("error");
+        avisoHelados.classList.remove("exito");
+
+    } else {
+
+        avisoHelados.textContent = "Pedido anotado - te atiende Mauricio Gustavo Aparicio Muñoz";
+        avisoHelados.classList.add("exito");
+        avisoHelados.classList.remove("error");
+
+    }
+}
+
+formularioHelados.addEventListener("submit", revisarPedido);
